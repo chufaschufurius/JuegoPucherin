b07d256a7b08b2728ac11bfa2a91dccb171c6fd0
CHUFASCHIN <jmmadridd@gmail.com>
2015-03-13 16:36:24 +0100

    Initial commit

---
+++ app/src/main/java/com/nicajaowk/javi/juegopucherin/Menu.java
@@ -0,0 +1,51 @@
+package com.nicajaowk.javi.juegopucherin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Menu extends ActionBarActivity {
+    Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b=(Button)findViewById(R.id.buttonJugar);
+    }
+    public void pulsarJugar(View view){
+        Intent intent=new Intent(this, Juega.class);
+//        Juego juego=new Juego(2);
+//        Bundle bunJuego=new Bundle();
+//        bunJuego.putSerializable("juego",juego);
+//        intent.putExtra("juego", bunJuego);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
